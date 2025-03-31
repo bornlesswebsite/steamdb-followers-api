0807c0cfac595b39c14a9187da9514ede671d685
bornlesswebsite <tom@thebornless.com>
2025-03-31 16:59:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,13 +15,15 @@ app.get("/followers/:2411280", async (req, res) => {
 
   try {
     const { data } = await axios.get(url, {
-      headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Connection": "keep-alive"
-      }
-    });
+  headers: {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://steamdb.info/",
+    "Connection": "keep-alive"
+  }
+});
+
 
     const $ = cheerio.load(data);
     let followers = null;
